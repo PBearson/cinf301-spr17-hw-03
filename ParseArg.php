aa5aeb6c8dbe44ab5a0b4719ac20a1aa956cdca1
PBearson <peabryan95@gmail.com>
2017-02-06 14:31:21 -0500

    Don't know what the last change was but here it isgit add *!

--- ParseArg.php
+++ ParseArg.php
@@ -24,14 +24,24 @@ class ParseArg
 			if($val[0] == "-" and $val[1] != "-")
 			{
 				//A single-dash that is self-contained
+				//Example: '-v'
 				if($a[$i + 1] == null or $a[$i + 1][0] == "-")
 				{
 					$args[$val[1]] = 'true';
 				}
+				
 				//A single dash followed by a string
+				//Example: -T 4
 				else if(!strpos($a[$i + 1], ','))
 				{
 					$args[$val[1]] = $a[$i + 1];
+				}
+				
+				//A single dash followed by an array list of values
+				//Example: -l Bryan,Kim,Austin
+				else 
+				{
+					
 				}
 			}
 		}
